c27f0786c02a61c10b714362ceadb7e2365ba77c
Fenil <fenil@sixsense.ai>
2022-06-22 18:27:59 +0530

    Added place finding feature and api integration.

--- src/Pages/DataInfo.tsx
+++ src/Pages/DataInfo.tsx
@@ -1,30 +1,72 @@
 import { Box, Card, Grid, Typography } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import ReactMapGl from "react-map-gl";
+import type { MapboxStyle } from "react-map-gl";
+import { Marker } from "react-map-gl";
+import mapSyles from "../assets/mapStyle";
+import bbox from "@turf/bbox";
+import LineTurf from "turf-linestring";
+import queryString from "query-string";
 
 const DataInfo = () => {
-  const [viewPort, setviewPort] = useState({
-    latitude: 59.9139,
-    longitude: 10.7522,
-    zoom: 19,
-  });
+  const [coordinatesData, setCoordinatesData] = useState(null);
+  const changesArrayValue = (data: any) => {
+    return data.map((x) => {
+      if (Array.isArray(x)) {
+        return changesArrayValue(x);
+      } else {
+        return parseFloat(x);
+      }
+    });
+  };
+
+  const handleRefChange = useCallback((node) => {
+    // const testData =
+    //   "SRID=4269;POLYGON ((10.9740691923595 59.238438059489, 10.9740227933371 59.2384118148634, 10.9739618408834 59.238440137309, 10.9739864838809 59.2384539566567, 10.9738577431111 59.2385136695373, 10.9738771411262 59.2385246938287, 10.9739431105353 59.2385619600357, 10.9739648580274 59.238574205259, 10.9741543790583 59.23848626153, 10.9741295513927 59.2384722665655, 10.9740691923595 59.238438059489))";
+    const params = queryString.parse(location.search);
+    if (params?.polygone) {
+      var regExp = /.*\(\((.*)\)\)/;
+      var matches = regExp.exec(params.polygone);
+
+      const coordinates = (matches?.[1] || "").split(", ").map((x) => x.split(" "));
+      const [longitude, latitude] = coordinates[0];
+      setCoordinatesData({ latitude, longitude });
+
+      const line = LineTurf(changesArrayValue(coordinates));
+      const [minLng, minLat, maxLng, maxLat] = bbox(line);
+
+      if (node) {
+        node.fitBounds(
+          [
+            [minLng, minLat],
+            [maxLng, maxLat],
+          ],
+          { padding: 40, duration: 1000 }
+        );
+      }
+    }
+  }, []);
 
   return (
-    <Box
-      height="100%"
-      sx={{ background: "#f1c9b7", padding: 10 }}
-      color="#da652e"
-    >
+    <Box height="100%" sx={{ background: "#f1c9b7", padding: 10 }} color="#da652e">
       <Grid container spacing={2}>
         <Grid item sm={5}>
           <Card sx={{ padding: 0, height: "700px" }}>
             <ReactMapGl
-              initialViewState={viewPort}
+              ref={handleRefChange}
+              initialViewState={{
+                latitude: 37.78,
+                longitude: -122.4,
+                zoom: 11,
+              }}
+              mapStyle={mapSyles as MapboxStyle}
+              interactiveLayerIds={["sf-neighborhoods-fill"]}
               mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
-              mapStyle="mapbox://styles/mapbox/streets-v9"
-              dragPan={false}
-              scrollZoom={false}
-            ></ReactMapGl>
+            >
+              {coordinatesData && (
+                <Marker longitude={coordinatesData.longitude} latitude={coordinatesData.latitude} />
+              )}
+            </ReactMapGl>
           </Card>
         </Grid>
         <Grid item sm={7}>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,17 @@
 // @ts-nocheck
 import { Autocomplete, Box, Button, CircularProgress, TextField } from "@mui/material";
-import { useState } from "react";
-import ReactMapGl, { Map } from "react-map-gl";
-import { getGeocoder } from "../api";
+import { useRef, useState } from "react";
+import ReactMapGl, { MapRef } from "react-map-gl";
+import { getGeocoder, getBoundingBox } from "../api";
 import { useNavigate } from "react-router";
-import GeocoderControl from "./GeoCoderControl";
 
 const accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 const Home = () => {
   const navigate = useNavigate();
-  const [viewPort, setviewPort] = useState({
-    latitude: 59.9139,
-    longitude: 10.7522,
+  const [viewport, setViewport] = useState({
+    latitude: 70.782223,
+    longitude: 22.308927,
     zoom: 10,
   });
   const [loading, setLoading] = useState(false);
@@ -20,31 +19,45 @@ const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [searchedValue, setSearchedValue] = useState({});
 
-  // const handleGeocoder = (e) => {
-  //   setLoading(true);
-  //   const params = {
-  //     types: "address",
-  //     limit: 10,
-  //     access_token: accessToken,
-  //   };
-  //   getGeocoder(e.target.value, params)
-  //     .then((res) => {
-  //       setOptions(
-  //         res?.data?.features.map((feature) => ({
-  //           label: feature.place_name,
-  //           ...feature,
-  //         }))
-  //       );
-  //       setLoading(false);
-  //     })
-  //     .catch(() => setLoading(false));
-  // };
+  const mapRef = useRef<MapRef>();
+
+  const handleGeocoder = (e) => {
+    setLoading(true);
+    const params = {
+      types: "address",
+      limit: 10,
+      access_token: accessToken,
+    };
+    getGeocoder(inputValue, params)
+      .then((res) => {
+        setOptions(
+          res?.data?.features.map((feature) => ({
+            label: feature.place_name,
+            ...feature,
+          }))
+        );
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  };
 
   const handleClick = () => {
     const option = options.find((opt) => opt.label === inputValue);
-    console.log({ option });
     setSearchedValue(option);
-    // const [latitude, longitude] = option?.geometry?.coordinates;
+    const [longitude, latitude] = option?.center;
+    mapRef.current?.flyTo({ center: [longitude, latitude], duration: 2000 });
+    getBoundingBox({
+      latitude,
+      longitude,
+      building_id: "",
+    }).then((res) => {
+      const { data } = res;
+      if (Array.isArray(data) && data.length > 0) {
+        const [placeData] = data;
+        navigate({ pathname: "/searched-info", search: `polygone=${placeData?.Shape_Area || ""}` });
+      }
+    });
+
     // setviewPort((prev) => {
     //   return { ...prev, latitude, longitude };
     // });
@@ -53,20 +66,15 @@ const Home = () => {
   return (
     <>
       <ReactMapGl
-        initialViewState={viewPort}
+        ref={mapRef}
+        initialViewState={viewport}
         mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
-        onViewPortChange={(viewport) => setviewPort(viewport)}
+        // onViewPortChange={(viewport) => setViewport(viewport)}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-        dragPan={false}
+        // dragPan={false}
         // scrollZoom={false}
-      >
-        <GeocoderControl
-          searchedValue={searchedValue}
-          mapboxAccessToken={accessToken}
-          position="top-left"
-        />
-      </ReactMapGl>
-      {/* <Box
+      ></ReactMapGl>
+      <Box
         display="flex"
         position="absolute"
         width="100%"
@@ -111,7 +119,7 @@ const Home = () => {
         >
           Search
         </Button>
-      </Box> */}
+      </Box>
     </>
   );
 };
